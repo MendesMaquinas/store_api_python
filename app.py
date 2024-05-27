280bdcf730d1bc710e5e9fa28736c5c552add46e
rafael.gois <api.mendes@mendesmaquinas.com.br>
2024-05-27 10:24:23 -0300

    up

---
+++ app.py
@@ -0,0 +1,68 @@
+import string
+import uuid
+
+from flask import Flask, request
+from flask_smorest import abort
+from db import items, stores
+
+app = Flask(__name__)
+
+
+@app.get("/stores")
+def get_stores():
+    return list(stores.values())
+
+
+@app.get("/stores/<string:store_id>")
+def get_stores_by_id(store_id):
+    try:
+        store = stores[store_id]
+        if store is not None:
+            return store
+    except KeyError:
+        return abort(404, message="Store not found")
+
+
+@app.post("/stores")
+def create_store():
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    new_store = {**store_data, "id": store_id}
+    stores[store_id] = new_store
+    return new_store, 201
+
+
+@app.get("/items")
+def get_items():
+    return list(items.values())
+
+
+@app.post("/items")
+def create_items():
+    item_data = request.get_json()
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(404, message="Body deve conter proce,store_id e name")
+
+    if item_data["store_id"] not in stores:
+        abort(400, message="Loja não encontrada")
+
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+
+    return item, 201
+
+
+@app.get("/items/<string:item_id>")
+def get_item(item_id):
+    try:
+        item = items[item_id]
+        if item is not None:
+            return item
+    except KeyError:
+        return abort(404, message="Item not found")
+
---
+++ db.py
@@ -0,0 +1,19 @@
+stores = {
+    "408baf8f63b147369d85292a2f2c3283": {
+        "id": "408baf8f63b147369d85292a2f2c3283",
+        "name": "Lojinha 1",
+    }
+}
+
+items = {
+    1: {
+        "name": "Iphone 15 PRO MAX",
+        "price": 10000.00,
+        "store_id": 98877476734
+    },
+    2: {
+        "name": "Macbook M4 PRO MAX",
+        "price": 50000.00,
+        "store_id": 98877476734
+    }
+}
