44f16304c4d75f7c3936b914c708940017061190
rafael.gois <api.mendes@mendesmaquinas.com.br>
2024-10-31 09:51:48 -0300

    up jwt

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import secrets
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 
@@ -10,7 +10,6 @@ from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
 from resources.user import blp as UserBlueprint
-
 
 def create_app(db_url=None):
     app = Flask(__name__)
@@ -28,8 +27,27 @@ def create_app(db_url=None):
 
     api = Api(app)
 
-    app.config["JWT_SECRET_KEY"] = secrets.SystemRandom().getrandbits(128)
+    app.config["JWT_SECRET_KEY"] = str(secrets.SystemRandom().getrandbits(30))
     jwt = JWTManager(app)
+
+    @jwt.additional_claims_loader
+    def add_claims_to_jwt(identity):
+        if identity == 1:
+            return {"is_admin": True}
+        return {"is_admin": False}
+
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return jsonify({"message": "O token expirou", "error": "token_expired"}), 401
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return jsonify({"message": "Assinatura inválida", "error": "invalid_token"}), 401
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return jsonify({"message": "A requisição não contém o token", "error": "authorization_required"}), 401
 
     with app.app_context():
         db.create_all()
--- resources/item.py
+++ resources/item.py
@@ -4,12 +4,14 @@ from schemas import ItemSchema, ItemUpdateSchema
 from models import ItemModel
 from db import db
 from sqlalchemy.exc import SQLAlchemyError
+from flask_jwt_extended import jwt_required, get_jwt
 
 blp = Blueprint("Items", "items", description="Operations on items")
 
 
 @blp.route("/item/<int:item_id>")
 class Item(MethodView):
+    @jwt_required()
     @blp.response(200, ItemSchema)
     def get(self, item_id):
         try:
@@ -18,12 +20,17 @@ class Item(MethodView):
         except KeyError:
             abort(404, message="Item not found.")
 
+    @jwt_required()
     def delete(self, item_id):
+        jwt = get_jwt()
+        if not jwt.get("is_admin"):
+            abort(401, message="Privilégio de administrador necessário")
         item = ItemModel.query.get_or_404(item_id)
         db.session.delete(item)
         db.session.commit()
         return {"message": "Item Deleted"}
 
+    @jwt_required()
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
@@ -43,13 +50,19 @@ class Item(MethodView):
 
     @blp.route("/item")
     class ItemList(MethodView):
+        @jwt_required()
         @blp.response(200, ItemSchema(many=True))
         def get(self):
             return ItemModel.query.all()
 
+        @jwt_required()
         @blp.arguments(ItemSchema)
         @blp.response(201, ItemSchema)
         def post(self, item_data):
+
+            if ItemModel.query.filter(ItemModel.name == item_data["name"]).first():
+                abort(400, message="Item já cadastrado")
+
             item = ItemModel(**item_data)
 
             try:
